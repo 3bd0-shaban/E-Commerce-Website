29a5e834dbab0aac0fbd17bc134223e1bb580457
AbdElra7man9 <abdelrahman201565@gmail.com>
2022-04-15 00:07:34 +0200

    the first upload in the project including the the frontend side

---
+++ models/signupuser.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+// const passportLocalMongoose = require("passport-local-mongoose");
+// const { object } = require("webidl-conversions");
+// const Schema = mongoose.Schema;
+ 
+// // define the Schema (the structure of the article)
+// const signupuser = new Schema({
+//   first_name: String,
+//   second_name: String,
+//   email:String,
+//   phonenumber:String,
+// });
+ 
+ 
+// // Create a model based on that schema
+// const user = mongoose.model("user", signupuser);
+ 
+ 
+// // export the model
+// module.exports = user; 
